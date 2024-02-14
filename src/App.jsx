5e24f7baf06a2fa03cfc3ee89b406683d342fffa
Yogesh gupta <yogeshgupta6524@gmail.com>
2024-02-14 15:10:20 +0530

    chnaged folder name in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import Header from "./components/headers/Header.jsx";
+import Header from "./components/header/Header";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./components/Home/Home.jsx";
 import Category from "./components/Category/Category.jsx";
