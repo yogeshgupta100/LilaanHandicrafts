56bda0132ead8b0657ec48acb3ffbc3b0a5c3e11
Yogesh gupta <yogeshgupta6524@gmail.com>
2024-02-14 15:16:50 +0530

    name changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import Header from "./components/header/Header";
-import Footer from "./components/Footer/Footer.jsx";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import Home from "./components/Home/Home.jsx";
 import Category from "./components/Category/Category.jsx";
 import SingleProduct from "./components/SingleProduct/SingleProduct.jsx";
-import Newsletter from "./components/Footer/Newsletter/Newsletter.jsx";
+import Newsletter from "./components/footer/Newsletter/Newsletter.jsx";
 import AppContext from "./utils/context.jsx";
 import About from "./components/About.jsx";
 
